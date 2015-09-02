0f8a7553851c1bbeaf0212732693f8d6cec2dc8c
Konstantin Bokarius <bokarius@comcast.net>
2015-09-02 09:57:30 -0700

    preliminary lib structure

---
+++ src/main/java/org/fanout/fanout/App.java
@@ -0,0 +1,13 @@
+package org.fanout.fanout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
